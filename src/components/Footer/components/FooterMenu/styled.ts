9f86d2b41bd44e9e1fe639cde2653817c46f4fee
ashibeko <ashibeko@gmail.com>
2024-01-15 22:14:33 +0300

    fix arrow styled

--- src/components/Footer/components/FooterMenu/styled.ts
+++ src/components/Footer/components/FooterMenu/styled.ts
@@ -2,30 +2,39 @@ import { getTextStyles } from "@autrm/common/utils/styles";
 import { styled } from "styled-components";
 
 export const ListStyled = styled.ul`
-    list-style: none;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+
+  & li {
     margin: 0;
     padding: 0;
+    margin-bottom: 8px;
+  }
 
-    & li {
-        margin: 0;
-        padding: 0;
-        margin-bottom: 8px;
-    }
+  & li a {
+    text-decoration: none;
+    color: #b3b3b3;
+    transition: .35s;
+    display: flex;
+    align-items: center;
+    gap: 7px;
+    transition: .35s;
 
-    & li a {
-        text-decoration: none;
-        color: #b3b3b3;
-        transition: .35s;
-        display: flex;
-        align-items: center;
-        gap: 7px;
-        transition: .35s;
+    ${ getTextStyles(null, 600, 15, null) }
+  }
 
-        ${ getTextStyles(null, 600, 15, null) }
-    }
+  & a svg {
+    opacity: 0;
+    transition: .35s;
+    fill: var(--primary-color1)
+  }
 
-    & li a:hover {
-        color: #fff;
-    }
+  & li a:hover {
+    color: #fff;
+  }
 
+  & li a:hover svg {
+    opacity: 1;
+  }
 `;
