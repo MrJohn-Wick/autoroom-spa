7cedbecd39c2ffeb487f74ee2ef46cac1123f078
John Wick <155922661+MrJohn-Wick@users.noreply.github.com>
2024-01-16 23:48:04 +0300

    Add lint config. (#5)

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  trailingComma: 'all',
+  arrowParens: 'always',
+  tabWidth: 2,
+  bracketSameLine: false,
+  bracketSpacing: true,
+  singleQuote: true,
+  semi: true,
+  singleAttributePerLine: true,
+  printWidth: 100,
+  endOfLine: 'auto',
+};
