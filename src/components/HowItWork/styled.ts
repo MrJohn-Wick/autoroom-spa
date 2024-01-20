b8e3503ffe1d51740e769b970db16953509d3d4c
Shibeko Alexander <ashibeko@gmail.com>
2024-01-20 19:13:42 +0300

    Steps ready.
    * steps icons and texts
    
    * steps icons and texts
    
    * steps ready
    
    * fix play icon

--- src/components/HowItWork/styled.ts
+++ src/components/HowItWork/styled.ts
@@ -1,8 +1,9 @@
 import { styled } from 'styled-components';
 
 import { Row } from '@autrm/common/components/grid';
-import { colorTitle } from '@autrm/common/tokens/colors';
+import { colorTitle, colorWhite } from '@autrm/common/tokens/colors';
 import { getTextStyles } from '@autrm/common/utils/styles';
+import { Button } from '@autrm/common/components/button';
 
 export const SectionStyled = styled.section`
   border-radius: 30px;
@@ -58,4 +59,8 @@ export const PilotStyled = styled.div`
   gap: 10px;
   padding-top: 30px;
   text-align: center;
+`;
+
+export const ButtonStyled = styled(Button)`
+  background-color: ${colorWhite};
 `;
