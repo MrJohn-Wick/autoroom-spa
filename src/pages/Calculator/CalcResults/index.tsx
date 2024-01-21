b5d9783bc6afc70b5a9891db0b1bed5ef7efbc40
ashibeko <ashibeko@gmail.com>
2024-01-21 23:55:01 +0300

    base markup

---
+++ src/pages/Calculator/CalcResults/index.tsx
@@ -0,0 +1,3 @@
+export function CalcResults() {
+  return <></>;
+}
--- src/pages/Calculator/index.tsx
+++ src/pages/Calculator/index.tsx
@@ -1,3 +1,30 @@
+import { Col, Container, Row } from '@autrm/common/components/grid';
+import { HeadingLevel1, RegularTextLevel2 } from '@autrm/common/tokens/typography';
+import { CalcForm } from './CalcForm';
+import { CalcResults } from './CalcResults';
+
 export function Calculator() {
-  return <div>#CalculatorPage</div>;
+  return (
+    <Container>
+      <Row mb="30px">
+        <Col lg={12}>
+          <div>
+            <HeadingLevel1>Калькулятор расходов</HeadingLevel1>
+            <RegularTextLevel2>
+              Все цены актуальны на момент расчета и могут измениться на момент доставки.В расчет не
+              входят комиссии за переводы.
+            </RegularTextLevel2>
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col lg={6}>
+          <CalcForm />
+        </Col>
+        <Col lg={6}>
+          <CalcResults />
+        </Col>
+      </Row>
+    </Container>
+  );
 }
