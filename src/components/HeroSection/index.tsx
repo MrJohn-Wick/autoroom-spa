401c38b31b6d03017f998309a8028e45dc617cd7
John Wick <155922661+MrJohn-Wick@users.noreply.github.com>
2024-02-14 08:12:48 +0300

    Fix for mobile view. Implement mobile view for Hero section. (#22)

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -1,20 +1,50 @@
 import iX from '@autrm/assets/bmw_ix.png';
+import { useWindowSize } from '@autrm/common/hooks/useWindowSize';
 import { Search } from '@autrm/components/Search';
 
-import { HeadingLevel1Styled, HeroSectionStyled, RegularTextLevel1Styled } from './styled';
+import { SearchMobile } from '../SearchMobile';
+
+import {
+  HeadingLevel1Styled,
+  HeroSectionStyled,
+  MobileSearchSectionStyled,
+  RegularTextLevel1Styled,
+} from './styled';
 
 export function HeroSection() {
+  const { isInfiniteDesktopView, isMiddleDesktopView, isTabletView, isMobileView } =
+    useWindowSize();
+
   return (
     <HeroSectionStyled>
-      <HeadingLevel1Styled>Авто из США</HeadingLevel1Styled>
-      <RegularTextLevel1Styled>Подберем авто Вашей мечты</RegularTextLevel1Styled>
-      <Search />
-      <img
-        src={iX}
-        width="1066"
-        height="336"
-        alt=""
-      />
+      {(isTabletView || isMiddleDesktopView || isInfiniteDesktopView) && (
+        <>
+          <HeadingLevel1Styled>Авто из США</HeadingLevel1Styled>
+          <RegularTextLevel1Styled>Подберем авто Вашей мечты</RegularTextLevel1Styled>
+          <Search />
+          <img
+            src={iX}
+            width="1066"
+            height="336"
+            alt=""
+          />
+        </>
+      )}
+      {isMobileView && (
+        <>
+          <HeadingLevel1Styled>Авто из США</HeadingLevel1Styled>
+          <RegularTextLevel1Styled>Подберем авто Вашей мечты</RegularTextLevel1Styled>
+          <MobileSearchSectionStyled>
+            <SearchMobile />
+          </MobileSearchSectionStyled>
+          <img
+            src={iX}
+            width="1066"
+            height="336"
+            alt=""
+          />
+        </>
+      )}
     </HeroSectionStyled>
   );
 }
--- src/components/SocialMenu/SocialButtons/Telegram/index.tsx
+++ src/components/SocialMenu/SocialButtons/Telegram/index.tsx
@@ -1,11 +1,12 @@
-import { SVGIcon } from '@autrm/common/components/svg-icon';
 import TelegramIcon from '@autrm/assets/icons/telegram.svg';
+import { SVGIcon } from '@autrm/common/components/svg-icon';
+
 import { TelegramButtonStyled } from './styled';
 
 export function Telegram({ isOpen }: { isOpen: boolean }) {
   return (
     <TelegramButtonStyled isOpen={isOpen}>
-      <a href="https://telegram.org">
+      <a href="https://t.me/autoroomby">
         <SVGIcon
           type={TelegramIcon}
           sizes={{
