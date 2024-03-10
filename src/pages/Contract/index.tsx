48a1231e6a07cab98efdd995541ff930592a7bbc
ashibeko <ashibeko@gmail.com>
2024-03-10 19:41:45 +0300

    change contract uri

--- src/pages/Contract/index.tsx
+++ src/pages/Contract/index.tsx
@@ -6,7 +6,7 @@ export function Contract() {
       <Row>
         <Col>
           <iframe
-            src="https://view.officeapps.live.com/op/embed.aspx?src=http%3A%2F%2Fieee802%2Eorg%3A80%2Fsecmail%2FdocIZSEwEqHFr%2Edoc"
+            src="https://view.officeapps.live.com/op/embed.aspx?src=http://alexhornet.vh71.hosterby.com/contract.doc"
             width="100%"
             height="700px"
           />
