9448c51af59aeb65fa774b337d49544080646a06
ashibeko <ashibeko@gmail.com>
2024-03-10 14:24:30 +0300

    test ms renderer

--- src/pages/Contract/index.tsx
+++ src/pages/Contract/index.tsx
@@ -1,40 +1,24 @@
 import { Container, Row, Col } from '@autrm/common/components/grid';
+import DocViewer, { MSDocRenderer, PNGRenderer } from '@cyntler/react-doc-viewer';
 
 export function Contract() {
+  const docs = [
+    {
+      uri: '/logo512.png',
+    },
+    {
+      uri: require('./test.docx'),
+    },
+  ];
+
   return (
     <Container>
       <Row>
         <Col>
-          <h1
-            style={{
-              textAlign: 'center',
-              marginBottom: '50px',
-            }}
-          >
-            ДОГОВОР №_____
-          </h1>
-          <div
-            style={{
-              display: 'flex',
-            }}
-          >
-            <div>г. Витебск, Республика Беларусь</div>
-            <div
-              style={{
-                display: 'flex',
-                justifyContent: 'end',
-              }}
-            >
-              «22» февраля 2024г.
-            </div>
-          </div>
-          <p>
-            Гражданин (ка)____________________________, именуемый(ая) в дальнейшем «Заказчик», с
-            одной стороны и Частное торговое унитарное предприятие «ВитЛайкКофе», именуемое в
-            дальнейшем «Исполнитель», в лице директора Кагукина Виталия Александровича, действующего
-            на основании Устава, с другой стороны, а вместе именуемые в дальнейшем «Стороны»,
-            заключили настоящий договор о нижеследующем:
-          </p>
+          <DocViewer
+            documents={docs}
+            pluginRenderers={[MSDocRenderer, PNGRenderer]}
+          />
         </Col>
       </Row>
     </Container>
