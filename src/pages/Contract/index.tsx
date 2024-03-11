6e691955a7770731979458f30fb282c0769338e3
Shibeko Alexander <ashibeko@gmail.com>
2024-03-11 22:05:32 +0300

    Contract
    * test ms renderer
    
    * test ms viewer
    
    * change contract uri
    
    * contract variants

--- src/pages/Contract/index.tsx
+++ src/pages/Contract/index.tsx
@@ -1,40 +1,16 @@
-import { Container, Row, Col } from '@autrm/common/components/grid';
+import { Row, Col, Container } from '@autrm/common/components/grid';
+// import { GoogleDocViewer } from './components/GoogleDocViewer';
+// import { DocViewer } from './components/DocViewer';
+import { PdfViewer } from './components/PdfViewer';
 
 export function Contract() {
   return (
     <Container>
       <Row>
         <Col>
-          <h1
-            style={{
-              textAlign: 'center',
-              marginBottom: '50px',
-            }}
-          >
-            ДОГОВОР №_____
-          </h1>
-          <div
-            style={{
-              display: 'flex',
-            }}
-          >
-            <div>г. Витебск, Республика Беларусь</div>
-            <div
-              style={{
-                display: 'flex',
-                justifyContent: 'end',
-              }}
-            >
-              «22» февраля 2024г.
-            </div>
-          </div>
-          <p>
-            Гражданин (ка)____________________________, именуемый(ая) в дальнейшем «Заказчик», с
-            одной стороны и Частное торговое унитарное предприятие «ВитЛайкКофе», именуемое в
-            дальнейшем «Исполнитель», в лице директора Кагукина Виталия Александровича, действующего
-            на основании Устава, с другой стороны, а вместе именуемые в дальнейшем «Стороны»,
-            заключили настоящий договор о нижеследующем:
-          </p>
+          {/* <DocViewer /> */}
+          <PdfViewer />
+          {/* <GoogleDocViewer /> */}
         </Col>
       </Row>
     </Container>
